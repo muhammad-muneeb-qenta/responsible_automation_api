3b38623e71105b9f541074f9e28ef7c156b89e26
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-05-31 11:13:30 +0500

    sa

--- test/auth/index.test.js
+++ test/auth/index.test.js
@@ -1,13 +1,13 @@
 describe('POST | Auth Endpoints', function() {
 
-    // it('should give valid token', function() {
-    //     var auth = require('./login-valid-payload.js');
-    //     return auth();
-    // });
-    // it('should give message "email or password is not valid"', function() {
-    //     var auth = require('./login-bad-password.js');
-    //     return auth();
-    // });
+    it('should give valid token', function() {
+        var auth = require('./login-valid-payload.js');
+        return auth();
+    });
+    it('should give message "email or password is not valid"', function() {
+        var auth = require('./login-bad-password.js');
+        return auth();
+    });
     it('should give message "Unable to verify Authorization Header"', function() {
         var auth = require('./login-invlaid-authtoken');
         return auth();
