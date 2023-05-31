e6375a2af5ed4f048a72667267fbf02b0b2202e1
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-05-31 10:27:34 +0500

    asd

--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,35 @@
-// Tests of Auth
-require('./auth/index.test.js');
+// // Tests of Auth
+// require('./auth/index.test.js');
 
-// Tests for GET User
-require('./users/index.test.js');
+// // Tests for GET User
+// require('./users/index.test.js');
 
-// Tests for GET Report EndPoint
-require('./reports/index.test.js');
+// // Tests for GET Report EndPoint
+// require('./reports/index.test.js');
 
 // Tests for GET Org and create Org
 require('./organization/index.test.js');
 
-// Tests for GET Product
-require('./product/index.test.js');
+// // Tests for GET Product
+// require('./product/index.test.js');
 
-// Tests for Create Product
-require('./product/createProduct/index.test.js');
+// // Tests for Create Product
+// require('./product/createProduct/index.test.js');
 
-// Tests for Retire/Restore Product
-require('./product/retireRestoreProduct/index.test.js');
+// // Tests for Retire/Restore Product
+// require('./product/retireRestoreProduct/index.test.js');
 
-// Tests for Get ESG Template
-require('./documents/getESGTemplate/index.test.js');
+// // Tests for Get ESG Template
+// require('./documents/getESGTemplate/index.test.js');
 
-// Tests for Get ESG Accreditation Data and Categories
-require('./documents/getAccreditationAndCategories/index.test.js');
+// // Tests for Get ESG Accreditation Data and Categories
+// require('./documents/getAccreditationAndCategories/index.test.js');
 
-// Tests for POST ESG Accreditation Data and Categories
-require('./documents/postAccreditationAndcategories/index.test.js');
+// // Tests for POST ESG Accreditation Data and Categories
+// require('./documents/postAccreditationAndcategories/index.test.js');
 
-// Tests for User Invitations
-require('./invitations/index.test.js');
+// // Tests for User Invitations
+// require('./invitations/index.test.js');
 
-// Tests for Asset Creation
-require('./assets/index.test.js');
+// // Tests for Asset Creation
+// require('./assets/index.test.js');
