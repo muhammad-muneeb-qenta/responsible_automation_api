c14985e22e0eeb74e809ebc0e65759d9862907b7
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-06-07 13:15:14 +0500

    ytr

---
+++ test/config.test.js
@@ -0,0 +1,15 @@
+// config.test.js
+
+const config = {
+    database: {
+      host: 'localhost',  // Mock or stub database host
+      port: 26004,        // Mock or stub database port
+      username: 'Admin',
+      password: 'P%40perpl%40ns786',
+      databaseName: 'develop',
+    },
+    // Other configuration options specific to testing environment
+  };
+  
+  module.exports = config;
+  
