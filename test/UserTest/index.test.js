6df8c7ed2298293153cfa18ef83f865df32cd8cd
Muhammad Muneeb <muhammad.muneeb@qenta.com>
2023-06-08 18:27:05 +0500

    asa

--- test/UserTest/index.test.js
+++ test/UserTest/index.test.js
@@ -1,9 +1,9 @@
 describe('POST | Login Endpoints', function() {
 
-    it('should give valid token', function() {
-        var login = require('./Valid_login.js');
-        return login();
-    });
+    // it('should give valid token', function() {
+    //     var login = require('./Valid_login.js');
+    //     return login();
+    // });
 
     it('create usr', function() {
         var login = require('./createUser.js');
